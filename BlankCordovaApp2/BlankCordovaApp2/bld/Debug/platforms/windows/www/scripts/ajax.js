c8f541188e35ffc6fc4c8fee121a15d3bf0b4ae3
oekarahan <ozlemkarahan38@hotmail.com>
2014-11-27 13:56:08 +0100

    27.11.2014

---
+++ BlankCordovaApp2/BlankCordovaApp2/bld/Debug/platforms/windows/www/scripts/ajax.js
@@ -0,0 +1,14 @@
+﻿
+
+$(document).ready(function(){       // code direkt nach laden der seite starten
+
+    $('#zurück a, #content a').live('click', function(){ //click funtkion zuweisen
+        
+        var pageToLoad = $(this).attr('href'); // das ziel-link auslesen und zwischenspeichern
+
+        $('#content').load(pageToLoad); // dem div content den inhalt zuweisen 
+
+
+        return false;
+    });
+    });
