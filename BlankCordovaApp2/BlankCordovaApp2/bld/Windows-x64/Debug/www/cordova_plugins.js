956809b5cb1a39d6c85bade920e488291a7561b1
oekarahan <ozlemkarahan38@hotmail.com>
2015-01-27 17:29:07 +0100

    27.01.2015
    17:30uhr

--- BlankCordovaApp2/BlankCordovaApp2/bld/Windows-x64/Debug/www/cordova_plugins.js
+++ BlankCordovaApp2/BlankCordovaApp2/bld/Windows-x64/Debug/www/cordova_plugins.js
@@ -1,32 +1,7 @@
 ﻿cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/org.apache.cordova.network-information/www/network.js",
-        "id": "org.apache.cordova.network-information.network",
-        "clobbers": [
-            "navigator.connection",
-            "navigator.network.connection"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.network-information/www/Connection.js",
-        "id": "org.apache.cordova.network-information.Connection",
-        "clobbers": [
-            "Connection"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.network-information/src/windows8/NetworkInfoProxy.js",
-        "id": "org.apache.cordova.network-information.NetworkInfoProxy",
-        "merges": [
-            ""
-        ]
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{
-    "org.apache.cordova.network-information": "0.2.12"
-}
+{}
 // BOTTOM OF METADATA
 });
