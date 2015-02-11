48dc8cdfbe4ada44affa1504de7c3ee89f97f004
oekarahan <ozlemkarahan38@hotmail.com>
2015-02-11 12:49:28 +0100

    11.02.2015
    13uhr

--- BlankCordovaApp2/BlankCordovaApp2/bld/Ripple/Windows-x64/Debug/cordova_plugins.js
+++ BlankCordovaApp2/BlankCordovaApp2/bld/Ripple/Windows-x64/Debug/cordova_plugins.js
@@ -1,6 +1,34 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/org.apache.cordova.camera/www/CameraConstants.js",
+        "id": "org.apache.cordova.camera.Camera",
+        "clobbers": [
+            "Camera"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.camera/www/CameraPopoverOptions.js",
+        "id": "org.apache.cordova.camera.CameraPopoverOptions",
+        "clobbers": [
+            "CameraPopoverOptions"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.camera/www/Camera.js",
+        "id": "org.apache.cordova.camera.camera",
+        "clobbers": [
+            "navigator.camera"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.camera/www/CameraPopoverHandle.js",
+        "id": "org.apache.cordova.camera.CameraPopoverHandle",
+        "clobbers": [
+            "CameraPopoverHandle"
+        ]
+    },
+    {
         "file": "plugins/org.apache.cordova.network-information/www/network.js",
         "id": "org.apache.cordova.network-information.network",
         "clobbers": [
@@ -19,6 +47,7 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "org.apache.cordova.camera": "0.3.2",
     "org.apache.cordova.network-information": "0.2.12"
 }
 // BOTTOM OF METADATA
